cde8c761c1cbcdcdab5f1478366725f1350728bb
ZØST (Alex B) <alejandre.bonnin@gmail.com>
2017-10-03 13:12:32 +0200

    Update ventana.py

--- ventana.py
+++ ventana.py
@@ -1,10 +1,10 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    w = QWidget()
-    w.resize(500, 345)
-    w.move(400, 400)
-    w.setWindowTitle('Web Parser')
-    w.show()
-    sys.exit(app.exec_())
+    webparser = QApplication(sys.argv)
+    ventana = QWidget()
+    ventana.resize(500, 345)
+    ventana.move(400, 400)
+    ventana.setWindowTitle('Web Parser')
+    ventana.show()
+    sys.exit(webparser.exec_())
